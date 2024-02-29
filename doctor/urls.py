f38045293d4403bbf449f6146c76b708a8646824
jithinjose-sics <jithinjose.sics@gmail.com>
2024-02-29 12:00:29 +0530

    changed urls.py and allowed hosts

--- doctor/urls.py
+++ doctor/urls.py
@@ -4,7 +4,11 @@ from .views import doctor_list_create_view,login_api_view,view_doctors,delete_do
 
 urlpatterns = [
     path('signup', doctor_list_create_view, name='doctor-list-create'),
+    path('signup/', doctor_list_create_view, name='doctor-list-create'),
     path('login', login_api_view, name='api-login'),
+    path('login/', login_api_view, name='api-login'),
     path('view-all-doctors',view_doctors,name='view-all-doctors'),
+    path('view-all-doctors/',view_doctors,name='view-all-doctors'),
     path('doctors/<int:doctor_id>', delete_doctor, name='delete_doctor'),
+    path('doctors/<int:doctor_id>/', delete_doctor, name='delete_doctor'),  
 ]
--- videotoimage_app/urls.py
+++ videotoimage_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from videotoimage_app import views
 
 urlpatterns = [
+    path('upload_video', views.upload_video, name='upload_video'),
     path('upload_video/', views.upload_video, name='upload_video'),
 ]
